e384a7f5d0c77545d466561fcf1ca5272fa69899
TwigaX <j.k.reszka@gmail.com>
2022-12-14 16:50:16 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+# good practice save functions in separate file
+def get_todos(filepath = "Todolist.txt"):
+    """ Read the txt file and return list of to-do items""" #DOCSTRINGS
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath = "Todolist.txt"):
+    """ Write the to- do items in the txt file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
